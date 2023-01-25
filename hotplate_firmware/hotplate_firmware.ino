4c4307a47de34fc95adbb8c1db39e58e31c10727
akowalczyk18 <akowalczyk18@gmail.com>
2023-01-24 22:56:29 -0500

    significant code cleanup, implementation of setpoint, halfway towards PID implementation

--- hotplate_firmware/hotplate_firmware.ino
+++ hotplate_firmware/hotplate_firmware.ino
@@ -1,20 +1,19 @@
 #include <SPI.h>
 #include <Wire.h>
+#include <PID_v1.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+//Set up defines for screen
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 //Set up defines for input and output pins
-
 #define HEATER_OUT 22
 #define UP_BTN 18
 #define SEL_BTN 19
 #define DWN_BTN 20
 #define TEMP_IN A2
-
-
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // The pins for I2C are defined by the Wire-library. 
@@ -33,8 +32,9 @@
 
 
 //Function Defines
-float adcToCelcius(float);
+double adcToCelcius(double);
 void error();
+void writeToDisplay(double tempIn, double tempSet, int heaterState);
 
 //ISR defines
 void upBtnISR();
@@ -43,9 +43,15 @@ void dwnBtnISR();
 
 
 
-//define floats used for temperature handling
-float RT, VR, ln, TX, T0, VRT;
-float tempSet = 150;
+//define doubles used for temperature handling
+double RT, VR, ln, TX, T0, VRT;
+double tempSet = 150;
+
+//define doubles used for PID
+
+double pidSetpoint;
+double pidInput;
+double pidOutput;
 
 //define flags to be set for buttons
 int upFlag = 0;
@@ -56,39 +62,55 @@ int heaterState = LOW;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+//PID Setup
+//Specify the links and initial tuning parameters
+double Kp=2, Ki=5, Kd=1;
+PID myPID(&pidInput, &pidOutput, &pidSetpoint, Kp, Ki, Kd, DIRECT);
 
+int WindowSize = 5000;
+unsigned long windowStartTime;
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-Wire.setSDA(0);
-Wire.setSCL(1);
-
- if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-
-T0 = 25 + 273.15; // set up T0 as variable
-//Clear display from Adafruit logo (loaded in by default)
+    // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.setSDA(0);
+  Wire.setSCL(1);
   
-display.clearDisplay();
+  
+   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;); // Don't proceed, loop forever
+    }
+  
+  T0 = 25 + 273.15; // set up T0 as variable
+  //Clear display from Adafruit logo (loaded in by default)
+    
+  display.clearDisplay();
+  
+  //Set up pin modes
+  pinMode(TEMP_IN, INPUT);
+  pinMode(HEATER_OUT, OUTPUT);
+  pinMode(UP_BTN, INPUT_PULLUP);
+  pinMode(SEL_BTN, INPUT_PULLUP);
+  pinMode(DWN_BTN, INPUT_PULLUP);
 
-//Set up pin modes
-pinMode(TEMP_IN, INPUT);
-pinMode(HEATER_OUT, OUTPUT);
-pinMode(UP_BTN, INPUT_PULLUP);
-pinMode(SEL_BTN, INPUT_PULLUP);
-pinMode(DWN_BTN, INPUT_PULLUP);
+  //Set up interrupts on pins
+  attachInterrupt(UP_BTN, upBtnISR, FALLING );
+  attachInterrupt(SEL_BTN, selBtnISR, FALLING );
+  attachInterrupt(DWN_BTN, dwnBtnISR, FALLING );
 
-attachInterrupt(UP_BTN, upBtnISR, FALLING );
-attachInterrupt(SEL_BTN, selBtnISR, FALLING );
-attachInterrupt(DWN_BTN, dwnBtnISR, FALLING );
-
+  //Set up PID parameters
+  windowStartTime = millis();
+  pidSetpoint = 150;
+  myPID.SetOutputLimits(0, WindowSize);
+  myPID.SetMode(AUTOMATIC);
+  
 }
 
-void loop() {
-  int tempDisplayed = 0;
+void loop() 
+{
+  double tempDisplayed = 0;
+
 
   VRT = analogRead(TEMP_IN);
   if (VRT > 850)
@@ -99,32 +121,71 @@ void loop() {
   {
     
   }
+  VRT = analogRead(A2);
+  Serial.println(VRT);
+  tempDisplayed = adcToCelcius(VRT);
 
   if(selFlag == 1)
-  {
-    if(heaterState == LOW)
     {
-      heaterState = HIGH;
-    }
-    else
-    {
-      heaterState = LOW;
-    }
-    selFlag = 0;
-    delay(100);
-    digitalWrite(HEATER_OUT, heaterState);
-  
-  }
+      if(heaterState == LOW)
+      {
+        heaterState = HIGH;
+        delay(100);
+      }
+      else
+      {
+        heaterState = LOW;
+        delay(100);
+      }
+      selFlag = 0;
+      delay(10);
+      digitalWrite(HEATER_OUT, heaterState);
+    } 
+   if(upFlag == 1)
+   {
+      if(tempSet < 250)
+        {
+          tempSet = tempSet + 1;
+          delay(100);
+        }
+      upFlag = 0;
+      delay(10);
+   }
+   if(dwnFlag == 1)
+     {
+     if(tempSet > 100)
+       {
+        tempSet = tempSet - 1;
+        delay(100);
+       }
+     dwnFlag = 0;
+     delay(10);
+     }
+  writeToDisplay(tempDisplayed, tempSet, heaterState);
+}
 
-  display.clearDisplay();
+
+double adcToCelcius(double tempIn)
+{
+  double tempOut;
+  tempIn = (3.3 / 1023.00) * tempIn;
+  VR = VCC - tempIn;
+  RT = tempIn / (VR / R);
+  ln = log(RT / RT0);
+  tempOut = (1 / ((ln / Beta) + (1 / T0)));
+  tempOut = tempOut - 273.15;
+  return tempOut;
+}
+
+//Display Update Function
+void writeToDisplay(double tempDisplayed, double tempSet, int heaterState)
+{
+    display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
   display.println(F("TEMPERATURE"));
   display.setTextSize(2);
-  VRT = analogRead(A2);
-  Serial.println(VRT);
-  tempDisplayed = adcToCelcius(VRT);
   display.setCursor(0, 10);
   display.println(tempDisplayed);
   display.setTextSize(1);
@@ -135,25 +196,16 @@ void loop() {
   }
   else
   {
-    display.println(F("HEAtER ON"));
+    display.println(F("HEATER ON"));
   }
+  display.setCursor(0, 40);
+  display.setTextSize(2);
+  display.println(tempSet);
   delay(100);
   display.display();
-  }
-
-
-float adcToCelcius(float tempIn)
-{
-  float tempOut;
-  tempIn = (3.3 / 1023.00) * tempIn;
-  VR = VCC - tempIn;
-  RT = tempIn / (VR / R);
-  ln = log(RT / RT0);
-  tempOut = (1 / ((ln / Beta) + (1 / T0)));
-  tempOut = tempOut - 273.15;
-  return tempOut;
 }
 
+//Button ISR Functions
 void upBtnISR() {
   upFlag = 1;
 }
